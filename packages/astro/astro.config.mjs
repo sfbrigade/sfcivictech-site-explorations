5a4fd8753cc1f9ef66d70615ebd415b3de7cd575
John Dunning <fw@johndunning.com>
2024-01-13 11:25:49 -0800

    Add GitHub workflow to deploy to Pages

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.sfcivictech.org/"
+	site: "https://sfbrigade.github.io",
+	base: "/sfcivictech-site-explorations/packages/astro",
+//	site: "https://www.sfcivictech.org/"
 });
