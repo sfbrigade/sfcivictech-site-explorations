f1073b0bfd812183d8111556b71c14268ddc7d9e
John Dunning <fw@johndunning.com>
2024-01-13 14:04:52 -0800

    Add <base> tag to fix URLs when hosted at a path
    Fix path handling in HeaderNav.astro.
    Fix titles on all pages.

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sfbrigade.github.io",
-	base: "/sfcivictech-site-explorations/packages/astro",
+	base: "/sfcivictech-site-explorations/packages/astro/",
 //	site: "https://www.sfcivictech.org/"
 });
