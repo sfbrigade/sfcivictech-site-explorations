6791b2ffd10ee71c96fb5748e022edc3ef58b1d5
John Dunning <fw@johndunning.com>
2024-01-13 17:04:24 -0800

    Switch the site base back to just /
    Even though the deployment is running from packages/astro, the pages are still output to / as the root.

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sfbrigade.github.io",
-	base: "/sfcivictech-site-explorations/packages/astro/",
+//	site: "https://sfbrigade.github.io",
+	base: "/",
+//	base: "/sfcivictech-site-explorations/packages/astro/",
 //	site: "https://www.sfcivictech.org/"
 });
