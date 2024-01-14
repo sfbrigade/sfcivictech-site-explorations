06fb313aeb4f4b6218b6165edde45ea7f2b327d3
John Dunning <fw@johndunning.com>
2024-01-13 17:11:01 -0800

    Change the base to /sfcivictech-site-explorations/
    On a personal repo, the repo name is the first part of the path.
    And use ./ for the home link in the nav header, so it respects the base.

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 //	site: "https://sfbrigade.github.io",
-	base: "/",
-//	base: "/sfcivictech-site-explorations/packages/astro/",
+//	base: "/",
+	base: "/sfcivictech-site-explorations/",
 //	site: "https://www.sfcivictech.org/"
 });
