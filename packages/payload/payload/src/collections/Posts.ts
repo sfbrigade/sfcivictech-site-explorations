c7938e9ba691c6eda56a126e16586dd8df6b18cc
John Dunning <fw@johndunning.com>
2024-01-20 23:22:07 -0800

    Update deps in packages/payload to latest
    Update payload.config.ts to work with Payload v2.
    Update README.md with info on payload package.
    Remove yarn.lock files for now, since updating the package versions made the lock files out of date.

--- packages/payload/payload/src/collections/Posts.ts
+++ packages/payload/payload/src/collections/Posts.ts
@@ -55,24 +55,24 @@ const Posts: CollectionConfig = {
     {
       name: "content",
       type: "richText",
-      admin: {
-        elements: ["h2", "h3", "h4", "link", "ol", "ul", "upload"],
-        leaves: ["bold", "italic", "underline"],
-        upload: {
-          collections: {
-            media: {
-              fields: [
-                {
-                  name: "imagel",
-                  type: "upload",
-                  relationTo: "media",
-                  required: true,
-                },
-              ],
-            },
-          },
-        },
-      },
+//      admin: {
+//        elements: ["h2", "h3", "h4", "link", "ol", "ul", "upload"],
+//        leaves: ["bold", "italic", "underline"],
+//        upload: {
+//          collections: {
+//            media: {
+//              fields: [
+//                {
+//                  name: "imagel",
+//                  type: "upload",
+//                  relationTo: "media",
+//                  required: true,
+//                },
+//              ],
+//            },
+//          },
+//        },
+//      },
     },
     {
       name: "status",
--- packages/payload/payload/src/payload.config.ts
+++ packages/payload/payload/src/payload.config.ts
@@ -1,26 +1,34 @@
-import { buildConfig } from "payload/config";
 import path from "path";
+import { buildConfig } from "payload/config";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
 import Posts from "@/collections/Posts";
 import Users from "@/collections/Users";
 import Media from "@/collections/Media";
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_URL,
-  admin: {
-    user: Users.slug,
-    webpack: (config) => ({
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@": path.resolve(__dirname, "./"),
-        },
-      },
-    }),
-  },
-  collections: [Posts, Users, Media],
-  typescript: {
-    outputFile: path.resolve("/", "types.ts"),
-  },
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URI,
+	}),
+	editor: slateEditor({}),
+	serverURL: process.env.PAYLOAD_URL,
+	admin: {
+		bundler: webpackBundler(),
+		user: Users.slug,
+		webpack: (config) => ({
+			...config,
+			resolve: {
+				...config.resolve,
+				alias: {
+					...config.resolve.alias,
+					"@": path.resolve(__dirname, "./"),
+				},
+			},
+		}),
+	},
+	collections: [Posts, Users, Media],
+	typescript: {
+		outputFile: path.resolve("/", "types.ts"),
+	},
 });
