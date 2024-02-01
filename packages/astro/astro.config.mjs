f7cf740d0ff3eb44523d13d826a3e97acc5cb179
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-01-31 18:37:04 -0800

    add icons to footer

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-//	site: "https://www.sfcivictech.org/"
-//	base: "/",
-//	site: "https://sfbrigade.github.io",
+	//	site: "https://www.sfcivictech.org/"
+	//	base: "/",
+	//	site: "https://sfbrigade.github.io",
 	base: "/sfcivictech-site-explorations/",
 	compressHTML: false,
+	integrations: [
+		icon({
+			include: {
+				// Include only three `mdi` icons in the bundle
+				// mdi: ['account', 'account-plus', 'account-minus'],
+				// Include all `mdi` icons in the bundle
+				mdi: ["*"],
+				// Include all `fa` icons
+				fa: ["*"],
+			},
+		}),
+	],
 });
