2cedfee9ae73fcd70ba4a88dbab0032fcfdba45e
NV <2823112+nickvisut@users.noreply.github.com>
2024-02-20 19:09:03 -0800

    add footer w/out icons (#8)
    * add footer nav WIP
    * tweak footer styles
    * add icons to footer
    * remove extraneous icons

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-//	site: "https://www.sfcivictech.org/"
-//	base: "/",
-//	site: "https://sfbrigade.github.io",
+	//	site: "https://www.sfcivictech.org/"
+	//	base: "/",
+	//	site: "https://sfbrigade.github.io",
 	base: "/sfcivictech-site-explorations/",
 	compressHTML: false,
+	integrations: [
+		icon({
+			include: {
+				// Include all `mdi` icons in the bundle
+				// mdi: ["*"],
+				// Include only specific `fa` icons in the bundle
+				fa: ["twitter", "facebook", "linkedin", "github", "slack", "meetup"],
+			},
+		}),
+	],
 });
